1ffaba658c260193a87de98628dabaa977c170c9
3-TA <77489816+3-TA@users.noreply.github.com>
2023-05-07 22:37:51 +0800

    Update SendInfo.py

--- SendInfo.py
+++ SendInfo.py
@@ -2,14 +2,16 @@ import smtplib
 import socket
 import getpass
 from json import load
-from urllib2 import urlopen
+from urllib.request import urlopen, Request
 
 import os, ssl
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
 getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-ipv6 = load (urlopen ('http://jsonip.com'))['ip']
+url = "http://jsonip.com"
+request_site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+ipv6 = load (urlopen (request_site))['ip']
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,8 +21,8 @@ s.close()
 
 un = socket.gethostname () + ".modem"
 
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 msg = MIMEMultipart()
 msg['From'] = 'th3ta403@gmail.com'
