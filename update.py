5f5942a83420e166d10f510f3d459ca1b6dadf68
3-TA <77489816+3-TA@users.noreply.github.com>
2023-11-16 04:43:54 +0800

    Delete update.py

--- update.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system ("""/usr/local/bin/python -c 'import urllib; import ssl; ssl._create_default_https_context = ssl._create_unverified_context; import time; time.sleep (1); urllib.urlretrieve ("https://github.com/3-TA/Loki/raw/main/moantroll.mp3", "/Users/Shared/moantroll.mp3")'""")
